5e9a899d25b27e3057295a6b3b9684f178d00859
seanybaggins <seli2854@colorado.edu>
2019-09-06 21:13:59 -0700

    Before trying to fix thread definitions

--- src/real_time_service.rs
+++ src/real_time_service.rs
@@ -1,33 +1,43 @@
-use scheduler;
-use std::thread;
+pub extern crate scheduler;
+pub use std::{thread, mem};
+pub use std::time::{Duration, Instant};
 
 pub const MAX_PRIORITY: i32 = 99;
 
 #[derive(Debug)]
 pub struct Task {
     period: u32,
-    runtime: u32,
-    thread_id: Option<thread::ThreadId>
+    priority: i32,
+    call_count: u32,
+    instructed_to_run: bool,
+}
+
+pub struct Sequencer {
+    task: Task,
+    max_call_count: u32,
 }
 
 impl Task {
 
     pub fn define_tasks() -> [Task; 3] {
-        
+
         let task1 = Task {
             period: 2,
-            runtime: 1,
-            thread_id: None
+            priority: MAX_PRIORITY - 1,
+            call_count: 0,
+            instructed_to_run: false,
         };
         let task2 = Task {
             period: 5,
-            runtime: 2,
-            thread_id: None
+            priority: MAX_PRIORITY - 2,
+            call_count: 0,
+            instructed_to_run: false,
         };
         let task3 = Task {
             period: 10,
-            runtime: 1,
-            thread_id: None
+            priority: MAX_PRIORITY - 3,
+            call_count: 0,
+            instructed_to_run: false,
         };
 
         return [task1, task2, task3]
